efbf49e65eda9e7c531b1353650b5b6dedd52c22
Sander Vispoel <sandervispoel@hotmail.com>
2018-06-19 18:33:53 +0200

    Fix withRouter TypeScript error

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -4,22 +4,17 @@ import { observer } from 'mobx-react';
 import { withRouter } from 'react-router';
 import { Test } from './modules';
 
-// @ts-ignore
-// Argument of type 'typeof App' is not assignable to parameter of type 'ComponentType<RouteComponentType<any>>'.
-@withRouter // necessary for App to re-render on route change
-@observer
-class App extends React.Component<AppProps, {}> {
-  render() {
-    return (
-      <main>
-        <Switch>
-          <Route exact path="/" component={Test}/>
-        </Switch>
-      </main>
-    );
-  }
-}
+const App: React.StatelessComponent<AppProps> = () => {
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/" component={Test}/>
+      </Switch>
+    </main>
+  );
+};
 
 export interface AppProps {}
 
-export default App;
+// withRouter is necessary for App to re-render on route change (MobX's fault)
+export default withRouter(observer(App));
