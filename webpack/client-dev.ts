aa883f5e972e5f766287c8336d24bfa41a87dec3
Sander <sander.vispoel@labela.nl>
2018-10-08 09:49:08 +0200

    Use port from config

--- webpack/client-dev.ts
+++ webpack/client-dev.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import * as ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { merge } from './base';
 import globals from './globals';
+import { port } from '../src/config';
 
 const devConfig: any = merge({
   name: 'client',
@@ -21,7 +22,7 @@ const devConfig: any = merge({
     noInfo: true,
     headers: { 'Access-Control-Allow-Origin': '*' },
     stats: { colors: true },
-    port: 3000,
+    port,
   },
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
